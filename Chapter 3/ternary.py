e1ca49a622ba552293c6a75df201fdfbbb948f99
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-20 08:52:45 +0600

    Ternary

---
+++ Chapter 3/ternary.py
@@ -0,0 +1,12 @@
+meaning = 40
+print('')
+
+# if (meaning < 30):
+#     print('Bigger')
+# else:
+#     print('Smaller')
+# print('')
+
+# Ternary Operator
+print('Bigger') if (meaning > 30) else ("Smaller")
+print('')
