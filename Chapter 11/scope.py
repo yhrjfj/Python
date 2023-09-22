76ab0e4e2288af05df1b213d078fc2f3a31ed394
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-22 10:58:12 +0600

    Scope

---
+++ Chapter 11/scope.py
@@ -0,0 +1,20 @@
+name = "Dave"
+count = 1
+
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("Dave")
+
+
+another()
